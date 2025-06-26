495648d33ccd1c41b99603d02dcd7c1edac7137e
gitbyul <gitbyul@gmail.com>
2025-06-26 10:14:19 +0900

    fix: update UserPayload type to use UserRole enum for role property

--- src/config/type/user-payload.type.ts
+++ src/config/type/user-payload.type.ts
@@ -1,6 +1,8 @@
+import { UserRole } from 'src/domain/auth/enums/user-role.enum';
+
 export type UserPayload = {
   id: string;
-  role: string;
+  role: UserRole;
   tokenType: string;
   iat: number;
   exp: number;
