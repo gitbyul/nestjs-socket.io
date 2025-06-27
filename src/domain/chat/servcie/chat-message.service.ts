9750e8fcf3190198011c77abac66eb75b004b86a
gitbyul <gitbyul@gmail.com>
2025-06-27 17:58:14 +0900

    feat: add methods for retrieving chat messages and message lists with transaction support in ChatMessageService

--- src/domain/chat/servcie/chat-message.service.ts
+++ src/domain/chat/servcie/chat-message.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { EntityManager, Repository } from 'typeorm';
+import { EntityManager, In, Repository } from 'typeorm';
 
 import { ChatMessages } from '../entity/ChatMessages.entity';
 import { ChatMessageType } from '../enums/chat-message-type.enum';
@@ -15,8 +15,12 @@ export class ChatMessageService {
 
   /**
    * 메시지 저장
-   * @param manager - 엔티티 매니저
-   * @param messageData - 메시지 데이터
+   * @param chatRoomId - 채팅방 ID
+   * @param templateId - 템플릿 ID
+   * @param message - 메시지
+   * @param type - 메시지 타입
+   * @param senderType - 발신자 타입
+   * @param senderId - 발신자 ID
    * @returns 저장된 메시지
    */
   async createWithTransaction(
@@ -39,5 +43,49 @@ export class ChatMessageService {
       senderId: messageData.senderId,
     });
     return await manager.save(ChatMessages, entity);
+  }
+
+  /**
+   * 메시지 조회
+   * @param chatRoomId - 채팅방 ID
+   * @param messageId - 메시지 ID
+   * @returns 메시지
+   */
+  async getChatMessageWithTransaction(
+    manager: EntityManager,
+    messageData: {
+      chatRoomId: string;
+      messageId: string;
+    },
+  ) {
+    const chatMessage = await manager.findOne(ChatMessages, {
+      where: {
+        id: messageData.messageId,
+        chatRoom: { id: messageData.chatRoomId },
+      },
+    });
+    return chatMessage;
+  }
+
+  /**
+   * 채팅방 메시지 목록 조회
+   * @param chatRoomId - 채팅방 ID
+   * @param messageIds - 메시지 ID 목록
+   * @returns 메시지 목록
+   */
+  async getChatMessageListWithTransaction(
+    manager: EntityManager,
+    messageData: {
+      chatRoomId: string;
+      messageIds: string[];
+    },
+  ) {
+    const chatMessage = await manager.find(ChatMessages, {
+      where: {
+        id: In(messageData.messageIds),
+        chatRoom: { id: messageData.chatRoomId },
+      },
+    });
+    return chatMessage;
   }
 }
