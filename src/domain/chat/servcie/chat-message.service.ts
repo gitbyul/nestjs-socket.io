0db3e4ec84447d2530b3f1563c620da83401a763
gitbyul <gitbyul@gmail.com>
2025-06-27 10:14:12 +0900

    feat: add createWithTransaction method in ChatMessageService for saving messages with transaction support

--- src/domain/chat/servcie/chat-message.service.ts
+++ src/domain/chat/servcie/chat-message.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 
 import { ChatMessages } from '../entity/ChatMessages.entity';
+import { ChatMessageType } from '../enums/chat-message-type.enum';
+import { UserRole } from 'src/domain/auth/enums/user-role.enum';
 
 @Injectable()
 export class ChatMessageService {
@@ -10,4 +12,32 @@ export class ChatMessageService {
     @InjectRepository(ChatMessages)
     private chatMessagesRepository: Repository<ChatMessages>,
   ) {}
+
+  /**
+   * 메시지 저장
+   * @param manager - 엔티티 매니저
+   * @param messageData - 메시지 데이터
+   * @returns 저장된 메시지
+   */
+  async createWithTransaction(
+    manager: EntityManager,
+    messageData: {
+      chatRoomId: string;
+      templateId?: string;
+      message: string;
+      type: ChatMessageType;
+      senderType: UserRole;
+      senderId: string;
+    },
+  ) {
+    const entity = ChatMessages.newMessage({
+      chatRoomId: messageData.chatRoomId,
+      templateId: messageData.templateId ?? undefined,
+      message: messageData.message,
+      type: messageData.type,
+      senderType: messageData.senderType,
+      senderId: messageData.senderId,
+    });
+    return await manager.save(ChatMessages, entity);
+  }
 }
