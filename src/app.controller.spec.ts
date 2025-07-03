579d96d99c623e7e013edd9aa23bb93e5d9bbde4
gitbyul <gitbyul@gmail.com>
2025-07-03 16:05:09 +0900

    test: enhance AppController tests by adding LogHttpInterceptor and LogUtil mocks for improved logging functionality

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LogHttpInterceptor } from './config/log/log-http.interceptor';
+import { LogUtil } from './config/log/log.util';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +10,26 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: LogUtil,
+          useValue: {
+            info: jest.fn(),
+            error: jest.fn(),
+            debug: jest.fn(),
+            HttpInfo: jest.fn(),
+            HttpSuccess: jest.fn(),
+            HttpError: jest.fn(),
+          },
+        },
+        {
+          provide: LogHttpInterceptor,
+          useValue: {
+            intercept: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
