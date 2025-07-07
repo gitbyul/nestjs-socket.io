5ae9e6f35b44cf7ef7c4582a2d77ff9e7446c8b6
gitbyul <gitbyul@gmail.com>
2025-07-07 18:05:17 +0900

    feat: extend environment validation schema to include AWS configuration variables

--- src/config/env/env.module.ts
+++ src/config/env/env.module.ts
@@ -4,13 +4,20 @@ import * as Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
   ENV: Joi.string().valid('dev', 'prod').required(),
+  // DB
   DB_TYPE: Joi.string().valid('mysql').required(),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().required(),
   DB_USERNAME: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
   DB_DATABASE: Joi.string().required(),
+  // JWT
   ACCESS_TOKEN_SECRET: Joi.string().required(),
+  // AWS
+  AWS_ACCESS_KEY_ID: Joi.string().required(),
+  AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+  AWS_REGION: Joi.string().required(),
+  AWS_S3_BUCKET_NAME: Joi.string().required(),
 });
 
 @Module({
