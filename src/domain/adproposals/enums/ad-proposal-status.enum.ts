ac5dfd3d27ea2c68ddf2cb3e4cf8ad7602e340a1
gitbyul <gitbyul@gmail.com>
2025-07-25 14:36:08 +0900

    fix: update comment for REVISION_REQUESTED status in AdProposalStatus enum for clarity and accuracy

--- src/domain/adproposals/enums/ad-proposal-status.enum.ts
+++ src/domain/adproposals/enums/ad-proposal-status.enum.ts
@@ -20,7 +20,7 @@ export enum AdProposalStatus {
   PUBLISHED = 'PUBLISHED', // 광고 게시됨(Only 작가)
 
   // 광고주, 작가
-  REVISION_REQUESTED = 'REVISION_REQUESTED', // 제안서수청(광고주가 작가에게 제안서 수정 요청)
+  REVISION_REQUESTED = 'REVISION_REQUESTED', // 제안서수정(광고주가 작가에게 제안서 수정 요청)
   ACCEPTED_BY_AUTHOR = 'ACCEPTED_BY_AUTHOR', // 작가 제안 수락(작가가 광고주의 제안을 수락)
   ACCEPTED_BY_ADVERTISER = 'ACCEPTED_BY_ADVERTISER', // 광고주 제안 수락(광고주가 작가의 제안을 수락)
   CONTRACT_COMPLETED_BY_AUTHOR = 'CONTRACT_COMPLETED_BY_AUTHOR', // 작가 계약 완료
