6a792ea47d05024cde18da7ddf7f3438501be052
gitbyul <gitbyul@gmail.com>
2025-07-25 09:20:46 +0900

    refactor: enhance HttpAuthGuard to include error logging and improve error handling during authentication process

--- src/domain/auth/guard/http-auth.guard.ts
+++ src/domain/auth/guard/http-auth.guard.ts
@@ -5,18 +5,27 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import { LogUtil } from 'src/config/log/log.util';
 
 @Injectable()
 export class HttpAuthGuard implements CanActivate {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly logger: LogUtil,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const user = await this.authService.authenticateHttp(request);
-    if (!user) {
-      throw new UnauthorizedException('User not found');
+    try {
+      const request = context.switchToHttp().getRequest();
+      const user = await this.authService.authenticateHttp(request);
+      if (!user) {
+        throw new UnauthorizedException('User not found');
+      }
+      request.user = user;
+      return true;
+    } catch (error) {
+      this.logger.error(`[AuthGuard][Error] ${error.message}`);
+      throw error;
     }
-    request.user = user;
-    return true;
   }
 }
